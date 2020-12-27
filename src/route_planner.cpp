91a6aea45787f9faf77f926d83f5c1caa1fc28c4
kangalioo <jannik.a.schaper@web.de>
2020-12-27 15:58:01 +0100

    fuck this absolute shit

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,7 +10,8 @@ void init_node(RouteModel::Node &node, RouteModel::Node *prev_node, RouteModel::
     node.FindNeighbors(); // fuck this hidden state-modifying shit
     if (prev_node) {
         node.parent = prev_node;
-        node.g_value = prev_node->g_value + 1;
+        node.g_value = prev_node->g_value + prev_node->distance(node);
+        // node.g_value = prev_node->g_value + 1;
     } else {
         node.parent = nullptr;
         node.g_value = 0;
@@ -19,22 +20,17 @@ void init_node(RouteModel::Node &node, RouteModel::Node *prev_node, RouteModel::
     node.visited = true; // I think??
 }
 
-// Returns the final path ordered from start to end, and the total distance.
-std::tuple<std::vector<RouteModel::Node>, float> construct_final_path(RouteModel::Node &end_node) {
-    auto distance = 0.0f;
-    // We hold pointers here instead of references because C++ is a hackfest and only support
-    // vectors of references with an ugly wrapper
+std::vector<RouteModel::Node> construct_final_path(RouteModel::Node &end_node) {
     std::vector<RouteModel::Node> path_found;
 
     RouteModel::Node &current_node = end_node;
     while (current_node.parent) {
         path_found.push_back(*current_node.parent);
-        distance += current_node.distance(*current_node.parent);
         current_node = *current_node.parent;
     }
     std::reverse(std::begin(path_found), std::end(path_found));
 
-    return {path_found, distance};
+    return path_found;
 }
 
 // Returns the final path ordered from start to end, and the total distance.
@@ -53,9 +49,26 @@ std::tuple<std::vector<RouteModel::Node>, float> plan_the_route(RouteModel &mode
             exit(1);
         }
         std::cout << "getting next node\n";
+
+        for (int i = 0; i < open_list.size() + 1; i++) {
+            auto n = open_list[i];
+            std::cout << "uhhh? " << n << "\n";
+            if ((size_t) n < 100) {
+                std::cout << "There's a fucking " << (size_t) n << " in here\n";
+            }
+        }
+        std::cout << "ok now: {";
+        for (auto elem : open_list) {
+            std::cout << elem << ", ";
+        }
+        std::cout << "}\n";
+
         // Get the open node with the least estimated total distance
         std::sort(open_list.begin(), open_list.end(), [](RouteModel::Node *a, RouteModel::Node *b){
-            return a->g_value + a->h_value > b->g_value + a->g_value;
+            std::cout << "wtfffffff " << a << " " << b << "\n";
+            if ((size_t) a < 0x550000000000 || (size_t) b < 0x550000000000) return true; // i dont even know
+            if ((size_t) a > 0x560000000000 || (size_t) b > 0x560000000000) return true; // i dont even FUCKING know
+            return a->g_value + a->h_value > b->g_value + b->h_value;
             // return a.g_value + a.h_value > b.g_value + a.g_value;
         });
         RouteModel::Node* optimal_node = open_list.back();
@@ -72,20 +85,22 @@ std::tuple<std::vector<RouteModel::Node>, float> plan_the_route(RouteModel &mode
                 std::cout << "already visited\n";
                 continue;
             }
-            std::cout << "not visited yet!\n";
+            std::cout << "not visited yet! adding " << neighbor << "\n";
             init_node(*neighbor, optimal_node, end_node);
             open_list.push_back(neighbor);
         }
         std::cout << "done with the shit\n";
     }
 
-    auto [path_found, distance] = construct_final_path(end_node);
-    distance *= model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
+    auto distance = end_node.g_value * model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
+    auto path_found = construct_final_path(end_node);
     return {path_found, distance};
 }
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model), start_x(start_x), start_y(start_y), end_x(end_x), end_y(end_y) {
-    std::cout << "Implemented.. I guess\n";
+    // FindClosestNode works with percentages
+    start_node = &model.FindClosestNode(start_x / 100, start_y / 100);
+    end_node = &model.FindClosestNode(end_x / 100, end_y / 100);
 }
 
 
